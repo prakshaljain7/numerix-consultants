1c66f9c4a98a665e76fbaeb9a3fea57f11a5a3cc
prakshal jain <prakshaljain7@gmail.com>
2025-07-04 21:36:47 +0530

    minor fixes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  basePath: '/numerix-consultants',
+  assetPrefix: '/numerix-consultants',
 };
 
 export default nextConfig;
